117474e3e6f70c6d63a33adc7eac8d34b1dc649d
SanchezAlexis <asanc183@itam.mx>
2021-09-09 20:42:10 -0500

    Agregue una suma
    Para practicar, agregue una metodo que hace una suma

--- hola.py
+++ hola.py
@@ -5,3 +5,10 @@ if __name__ == "__main__":
     print("Ejecutando cuando se invoca directamente")
 else:
     print("Ejecutado al ser importado")
+
+
+def suma(a,b):
+    return a+b 
+
+c=suma(5,8)
+print(c)
